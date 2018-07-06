c59c5b10e9157dfb5bbf2ceefc08dcdab6826b98
Shea Kusiak <shkusiak@gmail.com>
2018-07-06 17:21:36 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,7 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
   #has a catchphrase
+
+  def say_that_thing_you_say
+    puts "#{self.name} always says: #{self.catchphrase}"
 end
