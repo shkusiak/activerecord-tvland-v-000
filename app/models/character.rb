de7bcbf3e468227080c6555e5e60f9090c53a0dd
Shea Kusiak <shkusiak@gmail.com>
2018-07-06 17:08:00 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,11 +1,5 @@
 class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
-
-  def full_name
-    "#{self.first_name} #{self.last_name}"
-  end
-
-  def list_roles
-  end
+  
 end
