810f4a59baa89f1c0c1e589961e6577629a85735
Shea Kusiak <shkusiak@gmail.com>
2018-07-06 16:29:52 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
-      t.string :name
-      t.integer :show_id
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
